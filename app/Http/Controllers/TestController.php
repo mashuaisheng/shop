9cb5606605088835b3346359128f036a20687802
mss <1987886462@qq.com>
2020-10-07 16:35:42 +0800

    修改

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -8,8 +8,8 @@ class TestController extends Controller
     public function aa(){
     	$user = DB::table('news')->where('news_title', '垃圾')->first();
 		echo $user->news_title;
-		$email = DB::table('news')->where('news_title', '垃圾')->value('news_content');
-		echo $email;
+		// $email = DB::table('news')->where('news_title', '垃圾')->value('news_content');
+		// echo $email;
         // $users = DB::table('news')->get();
         // dd($users);
     }
